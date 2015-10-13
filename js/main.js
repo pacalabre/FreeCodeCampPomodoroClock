9db78287a6860cc21ad01e9fd29dc35ff08b537d
Paul <pacalabre@gmail.com>
2015-10-12 20:57:53 -0700

    added start and reset buttons

--- js/main.js
+++ js/main.js
@@ -1,31 +1,48 @@
 $(document).ready(function(){
   var mins = 25;
-  var seconds = 60;
-  //play sound
+  var secs = 1;
 
+  //play sound
   function playButton1() {
     var audioButton = new Audio();
-    audioButton.src = '.mp3';
+    audioButton.src = 'sound.mp3';
     audioButton.play();
   }
 
-  $('#audioButton').mousedown(function(){
-    playButton1();
-  });
+  //reset clock
+  function reset() {
+    mins = 25;
+    secs = 1;
+  }
+
+  // if reset button is pressed, reset the clock
+  $('#resetButton').mousedown(function(){
+    reset();
+  })
 
   //counter
-   var counter = function count() {
-      seconds-=1;
-      console.log(mins);
-      console.log(seconds);
-      setTimeout(count,1000);
-      if (seconds === 0) {
-        seconds = 60;
-        mins-=1;
-      }
-      }
-      counter();
+  var counter = function countdown() {
+    secs-=1;
+    setTimeout(countdown,1000);
 
+    console.log(mins);
+    console.log(secs);
 
+    // if seconds reach 0 -- reset secs to 60 and subtract a minute
+    if (secs === 0) {
+      secs = 60;
+      mins-=1;
+      }
+
+    // if mins reach 0 -- sound alarms
+    if (secs === 0 && mins === 0) {
+      playButton1();
+    }
+  }
+
+   //start counter
+   $('#startButton').mousedown(function(){
+    counter();
+  });
 
 });
